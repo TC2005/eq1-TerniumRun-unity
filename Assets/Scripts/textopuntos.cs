5671fae59baee567cc567ba66d7f8ae3f8438c92
Danielema2000 <a00227518@itesm.mx>
2021-04-23 06:57:45 -0700

    Gameplay terminado
    Ventana de gameover, puntuación, vida. Falta solo música, subir datos de partida a BD.

---
+++ Assets/Scripts/textopuntos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textopuntos : MonoBehaviour
+{
+    private Text msg;
+    private string textstr;
+    Datos_Partida datosPartida;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject target = GameObject.FindGameObjectWithTag("Datos");
+        datosPartida = target.GetComponent<Datos_Partida>();
+        msg = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        msg.text = (datosPartida.pdistancia + datosPartida.penemigos + datosPartida.ppreguntas).ToString();
+    }
+}
